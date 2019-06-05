0307f6146e425c47defd7185eadf0ce0a9f927d7
cradboard <cradboard@hotmail.com>
2019-06-05 18:32:40 -0400

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,15 @@
+//practice.js
+//variables
+var favColor = 'blue';
+var myFavColors = ['blue', 'red', 'green'];
+var numOfFavColors = 3;
+var hasGotFavColors = true;
+var richObject = {
+	firstName: 'Jordan',
+	lastName: 'Towers',
+	favColors: ['blue', 'red', 'green'],
+	yearsAlive: 22,
+	isMale: true
+}
+//end of variables
+//start of functions
